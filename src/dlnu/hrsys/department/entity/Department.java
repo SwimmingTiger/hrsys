9f3a683c1274394cef48dd686aaf198dacaa089f
王泽宇 <wzy@g.cn>
2016-06-29 10:20:40 +0800

    添加部门实体

---
+++ src/dlnu/hrsys/department/entity/Department.java
@@ -0,0 +1,62 @@
+package dlnu.hrsys.department.entity;
+
+public class Department {
+	private int id;
+	private String name;
+	private int type_id;
+	private String phone;
+	private String fax;
+	private String desc;
+	private String parent_id;
+	private String birth_date;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getType_id() {
+		return type_id;
+	}
+	public void setType_id(int type_id) {
+		this.type_id = type_id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getParent_id() {
+		return parent_id;
+	}
+	public void setParent_id(String parent_id) {
+		this.parent_id = parent_id;
+	}
+	public String getBirth_date() {
+		return birth_date;
+	}
+	public void setBirth_date(String birth_date) {
+		this.birth_date = birth_date;
+	}
+
+}
